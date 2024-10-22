fac58b29fa2e55a31bf4f9c1eece4b6ee71d371d
Zack <zack@ukaton.com>
2024-10-22 08:53:47 -0700

    .

---
+++ rollup.config.js
@@ -0,0 +1,237 @@
+import terser from "@rollup/plugin-terser";
+import MagicString from "magic-string";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
+import { dts } from "rollup-plugin-dts";
+import copy from "rollup-plugin-copy";
+import cleanup from "rollup-plugin-cleanup";
+
+const production = !process.env.ROLLUP_WATCH;
+
+function header() {
+  return {
+    renderChunk(code) {
+      code = new MagicString(code);
+
+      code.prepend(`/**
+ * @copyright Zack Qattan 2024
+ * @license MIT
+ */\n`);
+
+      return {
+        code: code.toString(),
+        map: code.generateMap(),
+      };
+    },
+  };
+}
+
+/** @param {"node" | "browser" | "ls"} context  */
+function removeLines(context) {
+  const isInBrowser = context == "browser";
+  const isInNode = context == "node";
+  const isInLensStudio = context == "ls";
+
+  return replace({
+    preventAssignment: true,
+    delimiters: ["", ""],
+    values: {
+      "/** BROWSER_START */": isInBrowser ? "" : "/*",
+      "/** BROWSER_END */": isInBrowser ? "" : "*/",
+      "/** NODE_START */": isInNode ? "" : "/*",
+      "/** NODE_END */": isInNode ? "" : "*/",
+      "/** LS_START */": isInLensStudio ? "" : "/*",
+      "/** LS_END */": isInLensStudio ? "" : "*/",
+    },
+  });
+}
+
+function replaceEnvironment() {
+  return replace({
+    preventAssignment: true,
+    values: {
+      __BRILLIANTSOLE__ENVIRONMENT__: JSON.stringify("__BRILLIANTSOLE__PROD__"),
+    },
+  });
+}
+
+const _plugins = [typescript(), cleanup({ comments: "none", extensions: ["js", "ts"] }), header()];
+
+if (production) {
+  _plugins.push(replaceEnvironment());
+}
+
+const _browserPlugins = [resolve(), commonjs(), removeLines("browser")];
+const _nodePlugins = [removeLines("node")];
+const nodeExternal = ["webbluetooth", "debounce", "ws", "@abandonware/noble", "auto-bind", "dgram"];
+
+const lensStudioPlugins = [
+  removeLines("ls"),
+  resolve(),
+  commonjs(),
+  babel({
+    babelHelpers: "bundled",
+    exclude: "node_modules/**",
+  }),
+];
+
+const name = "TS";
+const input = "src/TS.ts";
+
+const defaultOutput = {
+  sourcemap: true,
+  sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+    return relativeSourcePath.replace("../src/", "../tapstrap/");
+  },
+};
+
+const warningsToIgnore = ["PLUGIN_WARNING", "CIRCULAR_DEPENDENCY"];
+const onwarn = (warning) => {
+  if (warningsToIgnore.includes(warning.code)) {
+    return;
+  }
+  console.warn(warning);
+};
+
+const defaultBuild = {
+  watch: {
+    clearScreen: false,
+  },
+  onwarn,
+};
+
+const builds = [
+  {
+    input,
+    plugins: [..._browserPlugins, ..._plugins],
+    output: [
+      {
+        ...defaultOutput,
+        format: "esm",
+        file: "build/tapstrap.module.js",
+      },
+    ],
+    ...defaultBuild,
+  },
+  {
+    input: "./build/dts/TS.d.ts",
+    output: [{ file: "build/index.d.ts", format: "es" }],
+    plugins: [
+      removeLines("browser"),
+      dts(),
+      copy({
+        targets: [
+          { src: "build/index.d.ts", dest: "build", rename: "tapstrap.module.d.ts" },
+          { src: "build/index.d.ts", dest: "build", rename: "tapstrap.module.min.d.ts" },
+        ],
+      }),
+    ],
+    ...defaultBuild,
+  },
+
+  {
+    input,
+    plugins: [..._nodePlugins, ..._plugins],
+    external: nodeExternal,
+    output: [
+      {
+        ...defaultOutput,
+        format: "esm",
+        file: "build/tapstrap.node.module.js",
+      },
+    ],
+    ...defaultBuild,
+  },
+  {
+    input: "./build/dts/TS.d.ts",
+    output: [{ file: "build/index.node.d.ts", format: "es" }],
+    external: nodeExternal,
+    plugins: [
+      removeLines("node"),
+      dts(),
+      copy({
+        targets: [{ src: "build/index.node.d.ts", dest: "build", rename: "tapstrap.node.module.d.ts" }],
+      }),
+    ],
+    ...defaultBuild,
+  },
+];
+
+const productionOnlyBuilds = [
+  {
+    input,
+    plugins: [..._browserPlugins, ..._plugins, terser()],
+    output: [
+      {
+        ...defaultOutput,
+        format: "esm",
+        file: "build/tapstrap.module.min.js",
+      },
+    ],
+    ...defaultBuild,
+  },
+  {
+    input,
+    plugins: [..._browserPlugins, ..._plugins],
+    output: [
+      {
+        name,
+        ...defaultOutput,
+        format: "umd",
+        file: "build/tapstrap.js",
+        indent: "\t",
+      },
+    ],
+    ...defaultBuild,
+  },
+  {
+    input,
+    plugins: [..._browserPlugins, ..._plugins, terser()],
+    output: [
+      {
+        ...defaultOutput,
+        format: "umd",
+        name,
+        file: "build/tapstrap.min.js",
+      },
+    ],
+    ...defaultBuild,
+  },
+
+  {
+    input,
+    plugins: [..._nodePlugins, ..._plugins],
+    external: nodeExternal,
+    output: [
+      {
+        ...defaultOutput,
+        format: "cjs",
+        name,
+        file: "build/tapstrap.cjs",
+      },
+    ],
+    ...defaultBuild,
+  },
+  {
+    input,
+    plugins: [...lensStudioPlugins, ..._plugins],
+    output: [
+      {
+        ...defaultOutput,
+        format: "umd",
+        name,
+        file: "build/tapstrap.ls.js",
+      },
+    ],
+    ...defaultBuild,
+  },
+];
+
+if (production) {
+  builds.push(...productionOnlyBuilds);
+}
+
+export default builds;
---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import https from "https";
+import http from "http";
+const app = express();
+import fs from "fs";
+import ip from "ip";
+import * as TS from "./build/tapstrap.node.module.js";
+import nocache from "nocache";
+
+//BS.setAllConsoleLevelFlags({ log: false });
+
+// HTTPS SERVER
+app.use(nocache());
+app.use(function (req, res, next) {
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("x-frame-options", "same-origin");
+
+  next();
+});
+app.use(express.static("./"));
+
+const serverOptions = {
+  key: fs.readFileSync("./sec/key.pem"),
+  cert: fs.readFileSync("./sec/cert.pem"),
+};
+
+const httpServer = http.createServer(app);
+httpServer.listen(80);
+const httpsServer = https.createServer(serverOptions, app);
+httpsServer.listen(443, () => {
+  console.log(`server listening on https://${ip.address()}`);
+});
